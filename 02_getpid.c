71fed0e8d7a7de2990825005ae5928718ae6563a
Ged0oo <mohmmednagy@gmail.com>
2024-01-28 21:42:04 +0200

    add decumentation for getpid binary

--- 02_getpid.c
+++ 02_getpid.c
@@ -1,7 +1,12 @@
+/*
+ * This program simply gets the process Id and
+ * the parent process Id from the two system
+ * calls ==> getpid(), getppid()
+ */
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-
 
 void main(void)
 {
